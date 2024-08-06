17a2b4ad66ed095af8b293ca9fa289ca07bb4219
Rajeev jha <rajeevjha4141@gmail.com>
2024-08-07 00:42:01 +0530

    added different components  and done importing and exporting of components

---
+++ src/components/Body.js
@@ -0,0 +1,65 @@
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
+
+const Body = () => {
+  let listOfRestaurents = [
+    {
+      data: {
+        id: "800166",
+        name: "India Sweet House",
+        cloudinaryImageId: "a30547693c82a6fe583c3fedae6f4c6c",
+        locality: "Shivane",
+        areaName: "Basavanagudi",
+        costForTwo: "₹300 for two",
+        cuisines: ["Indian"],
+        avgRating: 4.4,
+      },
+    },
+    {
+        data: {
+            id: "800167",
+            name: "KFC",
+            cloudinaryImageId: "a30547693c82a6fe583c3fedae6f4c6c",
+            locality: "Shivane",
+            areaName: "Basavanagudi",
+            costForTwo: "₹300 for two",
+            cuisines: ["Indian"],
+            avgRating: 4.8,
+          },  
+    },
+    {
+        data: {
+            id: "800168",
+            name: "Baba ka Dhaba",
+            cloudinaryImageId: "a30547693c82a6fe583c3fedae6f4c6c",
+            locality: "Shivane",
+            areaName: "Basavanagudi",
+            costForTwo: "₹300 for two",
+            cuisines: ["Indian"],
+            avgRating: 3.8,
+          },
+    }
+  ];
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            listOfRestaurents = listOfRestaurents.filter((res) => res.data.avgRating > 4.5);
+            console.log(listOfRestaurents);
+          }}
+        >
+          Top Rated Restaurents
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurents.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
